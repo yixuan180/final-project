1dcf6c97ee6e48fc56fa6e3812857046e760a9ab
Rosaura0411 <kungsaingyi@gmail.com>
2025-05-07 22:30:33 +0800

    改名字2

--- config/urls.py
+++ config/urls.py
@@ -17,9 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from django.contrib import admin
-from django.urls import path, include
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('emotion/', include('emotion.urls')),  
