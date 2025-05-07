8720147bdabde8cd143e8dc8c27ad6fd64963c69
Rosaura0411 <kungsaingyi@gmail.com>
2025-05-07 21:54:49 +0800

    改名字

--- fate_dice/views.py
+++ fate_dice/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+import random
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+@csrf_exempt
+def roll_dice(request):
+    themes = {
+        "自然探索": ["陽明山健行", "烏來瀑布探訪", "北投溫泉步道"],
+        "在地文化": ["龍山寺參拜", "大稻埕古蹟巡禮", "艋舺剝皮寮歷史街區"],
+        "美食之旅": ["士林夜市小吃巡禮", "永康街牛肉麵", "迪化街古早味探索"],
+        "小眾秘境": ["象山秘境拍照", "深澳漁港日出", "青潭水庫步道"],
+    }
+
+    theme = random.choice(list(themes.keys()))
+    itinerary = themes[theme]
+
+    return JsonResponse({
+        "success": True,
+        "theme": theme,
+        "recommendations": itinerary,
+    })
