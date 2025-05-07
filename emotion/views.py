61bf1890bec0202447c3c264b03fff2c91600fae
Rosaura0411 <kungsaingyi@gmail.com>
2025-05-07 22:33:21 +0800

    改名字3

--- emotion/views.py
+++ emotion/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+# 在 emotion/views.py 裡加這段：
 
-# Create your views here.
+from django.http import JsonResponse
+
+def generate_by_emotion(request):
+    return JsonResponse({'message': '這是根據情緒生成的行程！'})
