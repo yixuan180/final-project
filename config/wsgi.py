39e34f299eb96ee7d4bcff92b6fec2bb5fcda1bb
Rosaura0411 <kungsaingyi@gmail.com>
2025-05-07 22:30:33 +0800

    改名字2

--- config/wsgi.py
+++ config/wsgi.py
@@ -10,7 +10,5 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'traveler_project1.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 application = get_wsgi_application()
